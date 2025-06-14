14a830017dbb5a6fa324bcfdd78ff50e4439b5c3
kittipop <jj-love-myz@hotmail.com>
2025-06-14 11:01:06 +0700

    Step 2: stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -55,8 +55,21 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     // //return Container(color: Colors.blueAccent , width 300, height: 300);
     // return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10));
-    return Container(color: Colors.blueAccent, 
-    margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
-    ) ;
+    // return Container(color: Colors.blueAccent, 
+    // margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
+    // ) ;
+    return Center(
+      child: Container(
+    color: Colors.blueAccent,
+    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+    padding: EdgeInsets.all(20),
+    child: Text(
+      "Hello Flutter",
+      style: TextStyle(
+        fontSize: 30, letterSpacing: 3),
+      ),
+    ),
+    );
+
   }
 }
