aa9a1358cdb4e8b90e7e315e072a99aa6d296177
kittipop <jj-love-myz@hotmail.com>
2025-06-14 11:28:42 +0700

    Step 4: stateless widget

---
+++ lib/screen/home.dart
@@ -0,0 +1,28 @@
+//Step 4: sprate the home widget into a separate file
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // //return Container(color: Colors.blueAccent , width 300, height: 300);
+    // return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10));
+    // return Container(color: Colors.blueAccent, 
+    // margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
+    // ) ;
+    return Center(
+      child: Container(
+    color: Colors.blueAccent,
+    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+    padding: EdgeInsets.all(20),
+    child: Text(
+      "Hello Flutter",
+      style: TextStyle(
+        fontSize: 30, letterSpacing: 3),
+      ),
+    ),
+    );
+
+  }
+}
